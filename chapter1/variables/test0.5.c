89284fa75cae3e0676b39a0798967f503b7528a9
bpricemg <43038187+bpricemg@users.noreply.github.com>
2023-09-26 08:38:13 -0400

    Updated 9/26

---
+++ chapter1/variables/test0.5.c
@@ -0,0 +1,19 @@
+/**
+ * This program will create variables, assign values, and
+ * print them out.
+ *
+ * Brandon Price
+ * CS50 AP
+ * Mount Greylock
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    // Declare an integer variable called x and assign the value
+    // 7 to it
+    int x = 7;
+
+    // Print out the current value
+    printf("x is %i\n", x);
+}
---
+++ chapter1/variables/test1.c
@@ -0,0 +1,23 @@
+/**
+ * This program will create variables, assign values, and
+ * print them out.
+ *
+ * Brandon Price
+ * CS50 AP
+ * Mount Greylock
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    // Declare an integer variable called x and assign the value
+    // 7 to it
+    int x = 7;
+
+    // Declare an integer variable called y and assign the value
+    // 6 to it
+    int y = 6;
+
+    // Print out the current value of x and y
+    printf("The value of x is %i and the value of y is %i\n", x, y);
+}
