4488f444247e27f098381fa5198158bfdfb5efd4
bpricemg <43038187+bpricemg@users.noreply.github.com>
2023-10-06 09:09:53 -0400

    Updated 10/6

---
+++ chapter1/cash/cash.c
@@ -0,0 +1,24 @@
+/**
+ * Determine the least number of coins needed to make change
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get the change owed from the user
+
+    // Convert the dollars and cents to just cents
+    int cents = round(dollars * 100);
+
+    // Determine the number of quarters needed
+
+    // Determine the number of dimes needed
+
+    // Determine the number of nickles needed
+
+    // Determine the number of pennies needed
+
+    // Print the result
+}
---
+++ chapter1/loops/silly.c
@@ -0,0 +1,13 @@
+// Demonstrates an infinite loop
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // This is an infinite loop that we can stop in the terminal
+    // with ctr + c
+    while(true)
+    {
+        printf("hello, world\n");
+    }
+}
