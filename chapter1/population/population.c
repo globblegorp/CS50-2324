d2ca6a0a97b2ddacecfa39daa39f6e1e80727c71
bpricemg <43038187+bpricemg@users.noreply.github.com>
2023-10-13 10:04:35 -0400

    Updated 10/13

---
+++ chapter1/population/population.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size (do-while loop)
+
+    // TODO: Prompt for end size (do-while loop)
+
+    // TODO: Calculate number of years until we reach threshold (while loop)
+
+    // TODO: Print number of years
+}
