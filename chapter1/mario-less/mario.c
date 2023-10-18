4f0f19ea3d1d346cb74d65cc1bcb0ac3af22cbc2
bpricemg <43038187+bpricemg@users.noreply.github.com>
2023-10-18 14:00:35 -0400

    Updated 10/18

--- chapter1/mario-less/mario.c
+++ chapter1/mario-less/mario.c
@@ -6,4 +6,8 @@ int main(void)
     // Prompt the user for the height, it should be between 1 and 8
 
     // Print the triangle
+    // For each row
+        // Print the number of spaces
+        
+        // Print the number of #
 }
