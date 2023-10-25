433118514bc30e7dd22520fc9c2c3b293c34fad8
bpricemg <43038187+bpricemg@users.noreply.github.com>
2023-10-25 14:23:09 -0400

    Updated 10/25

--- chapter2/functions/glocal.c
+++ chapter2/functions/glocal.c
@@ -10,9 +10,10 @@ int main(void)
     int x = get_int("Enter some integer value: ");
     printf("the value of x before adding 5 is %d\n", x);
 
-    // TODO: fix this so that the value of x is updated
-    add_5(x);
+    // fix this so that the value of x is updated
+    x = add_5(x);
     printf("the value of x after adding 5 is %d\n", x);
+    // printf("the value of x after adding 5 is %d\n", add_5(x));
 }
 
 // function definition
