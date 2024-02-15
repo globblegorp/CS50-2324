8d3605e4de28dff45e31611552e7c80d5d409279
bpricemg <43038187+bpricemg@users.noreply.github.com>
2024-02-15 13:46:55 -0500

    Updated 2/15

---
+++ chapter5/calculator.py
@@ -0,0 +1,11 @@
+from cs50 import get_int
+
+# Get two ints from the user
+x = get_int("x: ")
+y = get_int("y: ")
+
+# Add
+print(f"x plus y is {x + y}")
+
+# Division
+print(f"x divided by y is {x / y}")
